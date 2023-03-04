a61639ef20a99fcc56e76aefe65f36102ce9262d
Edelweiss1304 <smiledmitriev@gmail.com>
2023-03-05 00:34:54 +0500

    Подключаем соус

--- utilities/Conftest.py
+++ utilities/Conftest.py
@@ -1,14 +1,19 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver import DesiredCapabilities
+from selenium.webdriver.chrome.options import Options as ChromeOptions
 
 
 @pytest.fixture(scope="module")
 def driver():
-    options = webdriver.ChromeOptions()
+    options = ChromeOptions()
     options.add_experimental_option("detach", True)
     caps = DesiredCapabilities().CHROME
     caps["pageLoadStrategy"] = "eager"
+    sauce_options = {}
+    caps['sauce:options']['build'] = 'Edelweiss1304'
+    caps['sauce:options']['name'] = 'Strana'
+    options.set_capability('sauce:options', sauce_options)
     driver = webdriver.Chrome(desired_capabilities=caps, options=options)
     yield driver
     driver.quit()
