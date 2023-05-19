73ba1d9dc719293bf7f2f0f2785a9d88a1862d29
Edelweiss1304 <smiledmitriev@gmail.com>
2023-05-19 16:43:51 +0500

    Update

--- utilities/Conftest.py
+++ utilities/Conftest.py
@@ -1,12 +1,14 @@
 import pytest
 import datetime
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+from datetime import datetime
 
 
 @pytest.fixture(scope="function")
 def driver(request):
-    options = webdriver.ChromeOptions()
+    options = Options()
     options.add_experimental_option("detach", True)
     options.add_argument('--ignore-certificate-errors')
     options.add_argument("--headless")
@@ -14,11 +16,9 @@ def driver(request):
     caps["pageLoadStrategy"] = "eager"
     driver = webdriver.Chrome(desired_capabilities=caps, options=options)
     driver.set_window_size(1920, 1080)
-    yield driver
-    driver.quit()
 
     def fin():
-        if request.node.rep_call.failed:
+        if request.node.rep_setup.failed:
             driver.get_screenshot_as_file(
                 'screens/{}_{}.png'.format(driver.current_url.replace('/', '_'), datetime.now().isoformat()))
         driver.quit()
