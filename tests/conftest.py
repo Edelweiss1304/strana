b408a0e2da7471797095522f4de15dc3750c07ba
Edelweiss1304 <smiledmitriev@gmail.com>
2023-05-23 19:28:29 +0500

    Update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,52 +1,17 @@
 import pytest
-import datetime
 from selenium import webdriver
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-from selenium.webdriver.chrome.options import Options
-import os
 
-
-# @pytest.fixture(scope="function")
-# def driver():
-#     options = webdriver.ChromeOptions()
-#     options.add_experimental_option("detach", True)
-#     options.add_argument('--ignore-certificate-errors')
-#     options.add_argument("--headless")
-#     caps = DesiredCapabilities().CHROME
-#     caps["pageLoadStrategy"] = "eager"
-#     driver = webdriver.Chrome(desired_capabilities=caps, options=options)
-#     driver.set_window_size(1920, 1080)
-#     yield driver
-#     driver.quit()
 
 @pytest.fixture(scope="function")
-def driver(request):
-    options = Options()
+def driver():
+    options = webdriver.ChromeOptions()
     options.add_experimental_option("detach", True)
     options.add_argument('--ignore-certificate-errors')
-    #options.add_argument("--headless")
+    options.add_argument("--headless")
     caps = DesiredCapabilities().CHROME
     caps["pageLoadStrategy"] = "eager"
     driver = webdriver.Chrome(desired_capabilities=caps, options=options)
     driver.set_window_size(1920, 1080)
-
-    def fin():
-        # Check if setup or call phase has failed.
-        if hasattr(request.node, "rep_setup") and not request.node.rep_setup.passed:
-            failed = True
-        elif hasattr(request.node, "rep_call") and not request.node.rep_call.passed:
-            failed = True
-        else:
-            failed = False
-
-        if failed:
-            try:
-                if not os.path.exists("C:/home/strana_test/screens"):
-                    os.makedirs("C:/home/strana_test/screens")
-                driver.save_screenshot(f"C:/home/strana_test/screens/FAILURE_{request.node.name}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}.png")
-            except Exception as e:
-                print(f"Failed to create screenshot: {e}")
-        driver.quit()
-
-    request.addfinalizer(fin)
-    return driver
+    yield driver
+    driver.quit()
