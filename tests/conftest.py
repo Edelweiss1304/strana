e90a79d81cd04b5b11f8faa3c7cbfa9a54d466d6
Edelweiss1304 <smiledmitriev@gmail.com>
2023-05-31 11:08:46 +0500

    Update

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,7 +13,7 @@ def driver():
     options.add_argument('--ignore-certificate-errors')
     options.add_argument("--headless=new")
     options.add_argument('--no-sandbox')
-    options.add_argument('----disable-dev-shm-usage')
+    options.add_argument('--disable-dev-shm-usage')
     caps = DesiredCapabilities().CHROME
     caps["pageLoadStrategy"] = "eager"
     driver = webdriver.Chrome(desired_capabilities=caps, options=options)
