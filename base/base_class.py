6dc3010445dd8b8419ecdaa235b06365bccb253d
Edelweiss1304 <smiledmitriev@gmail.com>
2023-06-26 16:56:08 +0500

    Update

--- base/base_class.py
+++ base/base_class.py
@@ -1,5 +1,6 @@
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import StaleElementReferenceException, TimeoutException
 
 
 class Base:
@@ -12,15 +13,42 @@ class Base:
         driver.get(url)
 
     @classmethod
-    def get_element_visibility(cls, driver, locator, timeout=20):
-        return WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator))
+    def get_element_visibility(cls, driver, locator, timeout=20, max_attempts=5):
+
+        attempts = 0
+        while attempts < max_attempts:
+            try:
+                return WebDriverWait(driver, timeout).until(EC.visibility_of_element_located(locator))
+            except StaleElementReferenceException:
+                attempts += 1
+            except TimeoutException as e:
+                if attempts == max_attempts - 1:
+                    raise e
+                attempts += 1
+
+        raise StaleElementReferenceException(
+            "Couldn't find the element after several attempts."
+        )
 
     @classmethod
-    def get_element_clickable(cls, driver, locator, timeout=20):
-        return WebDriverWait(driver, timeout).until(EC.element_to_be_clickable(locator))
+    def get_element_clickable(cls, driver, locator, timeout=20, max_attempts=5):
+
+        attempts = 0
+        while attempts < max_attempts:
+            try:
+                return WebDriverWait(driver, timeout).until(EC.element_to_be_clickable(locator))
+            except StaleElementReferenceException:
+                attempts += 1
+            except TimeoutException as e:
+                if attempts == max_attempts - 1:
+                    raise e
+                attempts += 1
+
+        raise StaleElementReferenceException(
+            "Couldn't find the element after several attempts."
+        )
 
     @classmethod
     def get_s_link_wrapper_locator(cls, index):
         base_locator = "(//div[@class='s-link__wrapper'])"
         return f"{base_locator}[{index}]"
-    
