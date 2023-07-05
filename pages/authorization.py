986c865e1cf96b8327e03c9b3e5e6daccdbdef92
Edelweiss1304 <smiledmitriev@gmail.com>
2023-07-05 18:02:43 +0500

    Update

--- pages/authorization.py
+++ pages/authorization.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.by import By
 from pages.url import URLS_MAIN
 import pytest
 import time
+from pages.header_links import Header
 
 
 class Authorization(Base):
@@ -13,12 +14,12 @@ class Authorization(Base):
     enter_code_field = "//input[@id='input-56']"
     fin_login_btn = "//span[contains(text(),'Войти')]"
     check_lk = "//h1[contains(text(),'Брони и договоры')]"
+
+    first_login_broker_button = "//a[@class='v-btn v-btn--color-redline v-btn--has-bg v-btn--rounded v-btn--router v-btn--has-hover theme--light v-size--small button_EEpFj']"
     email_field_agent = "email"
-    password_field_agent = "//label[contains(text(), 'Пароль')]/following-sibling::input[@type='password']"
-    email_field_agency = "email"
-    password_field_agency = "//label[contains(text(), 'Пароль')]/following-sibling::input[@type='password']"
-    login_broker_btn = "//span[contains(text(),'Войти')]"
-    broker_agent_check = '//h1[contains(text(),"Клиенты")]'
+    password_field_agent = "//label[text()='Пароль']/following-sibling::input"
+    login_broker_btn = "//button[@type='submit']"
+    broker_agent_check = "//h1[contains(text(),'Сделки')]"
 
     # Getters
 
@@ -46,17 +47,14 @@ class Authorization(Base):
     def get_password_field_agent(self):
         return self.get_element_clickable(self.driver, (By.XPATH, self.password_field_agent))
 
-    def get_email_field_agency(self):
-        return self.get_element_clickable(self.driver, (By.NAME, self.email_field_agency))
-
-    def get_password_field_agency(self):
-        return self.get_element_clickable(self.driver, (By.XPATH, self.password_field_agency))
-
     def get_login_broker_btn(self):
         return self.get_element_clickable(self.driver, (By.XPATH, self.login_broker_btn))
 
     def get_broker_agent_check(self):
         return self.get_element_visibility(self.driver, (By.XPATH, self.broker_agent_check))
+
+    def get_first_login_broker_button(self):
+        return self.get_element_clickable(self.driver, (By.XPATH, self.first_login_broker_button))
 
     # Actions
 
@@ -76,6 +74,10 @@ class Authorization(Base):
         self.get_login_broker_btn().click()
         print('Нажата кнопка входа для брокера')
 
+    def click_first_login_broker_button(self):
+        self.get_first_login_broker_button().click()
+        print('Нажата кнопка входа для брокера на посадочной странице')
+
     # Methods
     def login_lk(self, phone_for_user=+79198629250, code_for_user=1313):
         time.sleep(1)
@@ -87,15 +89,37 @@ class Authorization(Base):
         self.get_enter_code_field().send_keys(code_for_user)
         self.click_fin_login_btn()
 
-    def login_lk_broker_agent(self, email='smiledmitriev@yandex.com', password='123456789'):
-        self.click_login_lk_button()
+    def login_page_broker_from_header(self, url):
+        head = Header(self.driver)
+        time.sleep(1)
+        head.actions.move_to_element(head.get_menu_button()).perform()
+
+        if url == 'https://mo.strana.com':
+            locator = Base.get_s_link_wrapper_locator(22)
+
+        elif url == 'https://nsk.strana.com':
+            locator = Base.get_s_link_wrapper_locator(19)
+
+        else:
+            locator = Base.get_s_link_wrapper_locator(24)
+
+        Base.get_element_visibility(self.driver, (By.XPATH, locator)).click()
+        self.click_first_login_broker_button()
+
+    def login_lk_broker(self, email='smiledmitriev@yandex.com', password='123456789'):
         self.get_email_field_agent().send_keys(email)
         self.get_password_field_agent().send_keys(password)
         self.click_login_broker_btn()
 
-    def login_lk_broker_agency(self, email='dmitrievaleks777@rambler.ru', password='1234567890'):
-        time.sleep(1)
-        self.click_login_lk_button()
-        self.get_email_field_agency().send_keys(email)
-        self.get_password_field_agency().send_keys(password)
-        self.click_login_broker_btn()
+    def login_broker_from_main_page(self, url):
+        if url == 'https://mo.strana.com':
+            locator = Base.get_s_link_wrapper_locator(12)
+
+        elif url == 'https://nsk.strana.com':
+            locator = Base.get_s_link_wrapper_locator(8)
+
+        else:
+            locator = Base.get_s_link_wrapper_locator(17)
+
+        Base.get_element_visibility(self.driver, (By.XPATH, locator)).click()
+        self.click_first_login_broker_button()
--- tests/test_login_broker.py
+++ tests/test_login_broker.py
@@ -3,28 +3,30 @@ from base.base_class import Base
 from pages.url import URLS_MAIN
 import pytest
 import allure
+from pages.header_links import Header
 import time
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.common.action_chains import ActionChains
 
 
-# @pytest.mark.parametrize("url", URLS_MAIN.values())
-# @pytest.mark.allure_feature("Проверка авторизации в ЛК брокера как агент")
-# @allure.title("Проверка авторизации в ЛК брокера как агент")
-# def test_authorization_agent(driver, url):
-#     auth = Authorization(driver)
-#     Base.open_page(driver, url)
-#     auth.login_lk_broker_agent()
-#     assert auth.get_broker_agent_check().text == "Клиенты"
-#     print('Успешный вход в ЛК брокера, как агент')
-#
-#
-# @pytest.mark.parametrize("url", URLS_MAIN.values())
-# @pytest.mark.allure_feature("Проверка авторизации в ЛК брокера как агентство")
-# @allure.title("Проверка авторизации в ЛК брокера как агентство")
-# def test_authorization_agency(driver, url):
-#     auth = Authorization(driver)
-#     Base.open_page(driver, url)
-#     auth.login_lk_broker_agency()
-#     assert auth.get_broker_agent_check().text == "Клиенты"
-#     print('Успешный вход в ЛК брокера')
+@pytest.mark.parametrize("url", URLS_MAIN.values())
+@pytest.mark.allure_feature("Проверка авторизации в ЛК брокера через хедер")
+@allure.title("Проверка авторизации в ЛК брокера через хедер")
+def test_authorization_agent_from_header(driver, url):
+    auth = Authorization(driver)
+    Base.open_page(driver, url)
+    auth.login_page_broker_from_header(url)
+    auth.login_lk_broker()
+    assert auth.get_broker_agent_check().text == "Сделки"
+    print('Успешный вход в ЛК брокера, как агент')
+
+
+@pytest.mark.parametrize("url", URLS_MAIN.values())
+@pytest.mark.allure_feature("Проверка авторизации в ЛК брокера через главную страницу")
+@allure.title("Проверка авторизации в ЛК брокера через главную страницу")
+def test_authorization_agent_from_main_page(driver, url):
+    auth = Authorization(driver)
+    Base.open_page(driver, url)
+    time.sleep(1)
+    auth.login_broker_from_main_page(url)
+    auth.login_lk_broker()
+    assert auth.get_broker_agent_check().text == "Сделки"
+    print('Успешный вход в ЛК брокера')
