124125d98c712766b874dd35ac7eae6291712607
smile <smiledmitriev@gmail.com>
2023-09-19 18:34:59 +0500

    Update

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,7 +1,6 @@
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
-from flaky import flaky
 import testit
 import os
 
@@ -17,19 +16,12 @@ def driver():
     options.add_argument("--proxy-server='direct://'")
     options.add_argument("--proxy-bypass-list=*")
     options.add_argument("--start-maximized")
-    # options.add_argument('--disable-gpu')
-    # options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--no-sandbox')
     options.set_capability("pageLoadStrategy", "eager")
-    service = Service()  # You need to set the path to your chromedriver
+    service = Service()
     driver = webdriver.Chrome(service=service, options=options)
     yield driver
     driver.quit()
-
-
-# def pytest_collection_modifyitems(config, items):
-#     for item in items:
-#         item.add_marker(pytest.mark.flaky(reruns=4, min_passes=1))
 
 
 @pytest.hookimpl(tryfirst=True)
@@ -37,10 +29,9 @@ def pytest_exception_interact(node, call, report):
     if call.when == "call" and report.failed:
         driver = node.funcargs["driver"]
         screenshot_folder = "screens"
-        os.makedirs(screenshot_folder, exist_ok=True)  # Создаем папку screens, если она не существует
+        os.makedirs(screenshot_folder, exist_ok=True)
         screenshot_path = os.path.join(screenshot_folder, f"screenshot_{report.nodeid.replace('/', '_')}.png")
         driver.save_screenshot(screenshot_path)
         testit.addAttachments(screenshot_path)
-        # Выводим путь к скриншоту в консоль для наглядности
         print(f"Скриншот сохранен: {screenshot_path}")
 
