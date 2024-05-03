efeb5f12b46b1decae3b6076ea03ab8465071e9b
smile <smiledmitriev@gmail.com>
2024-05-03 15:52:06 +0500

    Update

--- fail_tests.py
+++ fail_tests.py
@@ -7,12 +7,12 @@ def extract_failed_tests_names(report_file_path):
         report_json = json.load(report_file)
         for test in report_json.get('tests', []):
             if test['outcome'] == 'failed':
-                failed_tests_names.append(test['nodeid'])
-
+                failed_tests_names.append(str(test['nodeid']))
     return ", ".join(failed_tests_names)
 
 
-report_file = '.report.json'
-failed_tests = extract_failed_tests_names(report_file)
-
-print(failed_tests if failed_tests else "All tests passed")
+if __name__ == "__main__":
+    report_file = '.report.json'
+    failed_tests = extract_failed_tests_names(report_file)
+    with open('failed_tests.txt', 'w') as f:
+        f.write(failed_tests if failed_tests else "All tests passed")
